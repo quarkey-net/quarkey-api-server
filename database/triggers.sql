0beba02d0efe9e8894c22499681b2560c1f2686e
Estéban Ristich <esteban.ristich@protonmail.com>
2021-07-27 19:56:49 +0200

    Add triggers SQL file

---
+++ database/triggers.sql
@@ -0,0 +1,10 @@
+-- Author : Estéban Ristich <esteban.ristich@protonmail.com>
+
+CREATE TRIGGER trgg_auto_update_accounts
+    BEFORE UPDATE ON accounts FOR EACH ROW
+    EXECUTE PROCEDURE func_update_tmstmp();
+
+
+CREATE TRIGGER trgg_auto_update_passwords
+    BEFORE UPDATE ON passwords FOR EACH ROW
+    EXECUTE PROCEDURE func_update_tmstmp();
