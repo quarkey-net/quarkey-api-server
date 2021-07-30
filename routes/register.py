8148cdb0dfcf2f637d0283c719d36218f2db7bc6
Estéban Ristich <esteban.ristich@protonmail.com>
2021-07-30 07:38:49 +0200

    Update register with native SQL

--- routes/register.py
+++ routes/register.py
@@ -3,8 +3,6 @@ from utils.security.auth import UserPasswordHasher, gen_account_keypair
 from utils.config import AppState
 from utils.base import api_validate_form, api_message
 
-from database.models import SQLAccounts
-
 class Register(object):
 
     def __init__(self):
@@ -18,13 +16,14 @@ class Register(object):
             "$schema": AppState.Tools.JSONSCHEMA_VERSION,
             "type": "object",
             "properties": {
-                "uid"       : {"type": "string", "pattern": "^(?=[a-zA-Z0-9._]{4,15}$)(?!.*[_.]{2})[^_.].*[^_.]$"},
+                "id"       : {"type": "string", "pattern": "^(?=[a-zA-Z0-9._]{4,15}$)(?!.*[_.]{2})[^_.].*[^_.]$"},
                 "firstname" : {"type": "string"},
                 "lastname"  : {"type": "string"},
                 "email"     : {"type": "string", "format": "email"},
-                "password"  : {"type": "string", "pattern": "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,60}$"}
+                "password"  : {"type": "string", "pattern": "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,60}$"},
+                "key"       : {"type": "string"}
             },
-            "required": ["uid", "firstname", "lastname", "email", "password"]
+            "required": ["id", "firstname", "lastname", "email", "password", "key"]
         }
 
 
@@ -35,38 +34,52 @@ class Register(object):
         resp.status         = falcon.HTTP_400
 
         if api_validate_form(req.media, self.post_form):
-            try:
-                q1 = SQLAccounts.get_or_none((SQLAccounts.uid == req.media["uid"]) | (SQLAccounts.email == req.media["email"]))
-            except Exception as e:
-                api_message('w', f'Failed to request database : {e}')
-                resp.status = falcon.HTTP_500
-                return
-            
+            q1 = None
+            q2 = None
+            with AppState.Database.CONN.cursor() as cur:
+                cur.execute("SELECT * FROM tester_keys WHERE id = %s AND type = %s AND expiration_on > CURRENT_TIMESTAMP LIMIT 1", (req.media["key"], AppState.TAG))
+                q1 = cur.fetchone()
+
             if q1 is None:
+                api_message('d', "Invalid key")
+                resp.status = falcon.HTTP_400
+                resp.media = {'title': 'BAD_REQUEST', 'description': 'key not found'}
+                return
+
+            key_id: int = q1[0]
+            print(f'key id : {key_id}')
+
+            with AppState.Database.CONN.cursor() as cur:
+                cur.execute("SELECT * FROM accounts WHERE id = %s OR email = %s", (req.media['id'], req.media["email"]))
+                q2 = cur.fetchone()
+
+            if q2 is None:
                 keypair = gen_account_keypair()
                 datenow = datetime.datetime.utcnow()
-                try:
-                    SQLAccounts.create(
-                        uid=req.media['uid'],
-                        firstname=req.media['firstname'],
-                        lastname=req.media['lastname'],
-                        email=req.media['email'],
-                        password=self.password_hasher.hash_password(req.media['password']),
-                        public_key=keypair[0],
-                        private_key=keypair[1],
-                        role="standard",
-                        premium_expiration=datenow,
-                        updated_on=datenow,
-                        created_on=datenow
+
+                with AppState.Database.CONN.cursor() as cur:
+                    cur.execute(
+                        "INSERT INTO accounts (id, firstname, lastname, email, password, public_key, private_key, roles, f_tester_key) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                        (
+                            req.media['id'],
+                            req.media['firstname'],
+                            req.media['lastname'],
+                            req.media['email'],
+                            self.password_hasher.hash_password(req.media['password']),
+                            keypair[0],
+                            keypair[1],
+                            "standard",
+                            key_id
+                        )
                     )
-                    api_message('d', "successfull register (user=\"{0}\", uid={1})".format(req.media['firstname'] + ' ' + req.media['lastname'], req.media['uid']))
-                    resp.status = falcon.HTTP_201
-                    resp.media = {'title': 'CREATED', 'description': 'accounts created successfull'}
-                    return
-                except Exception as e:
-                    api_message('w', f'Failed to request database to create user : {e}')
-                    resp.status = falcon.HTTP_500
-                    return
+                AppState.Database.CONN.commit()
+            
+        
+                api_message('d', "successfull register (user=\"{0}\", uid={1})".format(req.media['firstname'] + ' ' + req.media['lastname'], req.media['id']))
+                resp.status = falcon.HTTP_201
+                resp.media = {'title': 'CREATED', 'description': 'accounts created successfull'}
+                return
             else:
                 resp.status = falcon.HTTP_400
                 resp.media = {'title': 'BAD_REQUEST', 'description': 'username or email already exist'}
+                return
