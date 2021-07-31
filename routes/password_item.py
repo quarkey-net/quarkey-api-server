1184bf7b7ed131fc76e43e1fec38792a18ad3ead
Estéban Ristich <esteban.ristich@protonmail.com>
2021-07-31 09:11:50 +0200

    Update password items endpoint to support native SQL

--- routes/password_item.py
+++ routes/password_item.py
@@ -3,7 +3,6 @@ import falcon, uuid, datetime
 
 from routes.middleware import AuthorizeAccount
 from utils.base import api_validate_form, api_message
-from database.models import SQLPasswords
 from utils.config import AppState
 
 
@@ -28,80 +27,65 @@ class PasswordItem(object):
     def on_post(self, req, resp):
         resp.status = falcon.HTTP_400
         if api_validate_form(req.media, self.post_form):
-            api_message("d", f'request media type : {type(req.media)}')
             payload = self._token_controller.decode(req.get_header('Authorization'))
+            q1 = None
+            with AppState.Database.CONN.cursor() as cur:
+                cur.execute("SELECT id FROM passwords WHERE f_owner = %s AND title = %s", (payload['uid'], req.media['title']))
+                q1 = cur.fetchone()
 
-            try:
-                q1 = SQLPasswords.select(
-                    SQLPasswords.id
-                ).where((SQLPasswords.f_owner == payload['uid']) & (SQLPasswords.title == req.media['title']))
-            except Exception as e:
-                api_message("d", f'failed to get item {e}')
-                raise falcon.HTTPBadRequest(title="BAD_REQUEST", description="failed to get item")
-
-            if q1.count() > 0:
+            if q1 is not None:
                 resp.status = falcon.HTTP_BAD_REQUEST
                 resp.media  = resp.media  = {"title": "BAD_REQUEST", "description": "item already exist"}
-                return
+                return                
 
-            datenow = datetime.datetime.utcnow()
-            try:
-                q2 = SQLPasswords.create(
-                    id=uuid.uuid4(),
-                    f_owner=payload['uid'],
-                    title=req.media['title'],
-                    description=req.media['description'],
-                    username=req.media['username'],
-                    password=req.media['password'],
-                    url=req.media['url'],
-                    updated_on=datenow,
-                    created_on=datenow
+            puuid = uuid.uuid4().hex
+            with AppState.Database.CONN.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO passwords (id, f_owner, name, description, username, password, url) VALUES (%s, %s, %s, %s, %s, %s, %s)", 
+                    (
+                        puuid,
+                        payload['uid'],
+                        req.media['title'],
+                        req.media['description'],
+                        req.media['username'],
+                        req.media['password'],
+                        req.media['url']
+                    )
                 )
-            except Exception as e:
-                api_message("d", f'exception during password item creation : {e}')
-                raise falcon.HTTPBadRequest(title="BAD_REQUEST", description="failed to create item")
+                cur.execute("INSERT INTO password_tag_linkers (f_password, f_tag) VALUES (%s, %s)", (puuid, None))
+                AppState.Database.CONN.commit()
+
             
         resp.status = falcon.HTTP_CREATED
         resp.media  = {"title": "CREATED", "description": "resource created successful"}
 
 
+    @falcon.before(AuthorizeAccount(roles=["standard"]))
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_BAD_REQUEST
+        payload = self._token_controller.decode(req.get_header('Authorization'))
+        q1 = None
+        columns = None
+        with AppState.Database.CONN.cursor() as cur:
+            cur.execute("SELECT t2.name, t2.description, t2.login, t2.password, t2.url, t4.name AS tag_name, t4.color AS tag_color FROM password_tag_linkers AS t1 INNER JOIN passwords AS t2 ON t1.f_password = t2.id INNER JOIN accounts AS t3 ON t2.f_owner = t3.id LEFT JOIN tags AS t4 ON t1.f_tag = t4.id WHERE t3.id = %s AND t3.is_banned = FALSE", (payload['uid'],))
+            columns = list(cur.description)
+            q1 = cur.fetchall()
 
-"""
-    @falcon.before(AuthorizeAccount(roles=['standard']))
-    def on_get(self, req, resp, uid):
-        resp.status = falcon.HTTP_400
-
-        premium = decode_user_token(req.get_header('Authorization'))["premium"]
-
-        try:
-            if "num" in req.params:
-                i_number = int(req.params.get("num"))
-                if i_number > 12 and premium != True:
-                    query = PasswordItems.select(PasswordItems.title,
-                        PasswordItems.description, 
-                        PasswordItems.username,
-                        PasswordItems.password
-                    ).where(PasswordItems.owner == uid).order_by(PasswordItems.created_date).limit(12).dicts()
-                else:
-                    query = PasswordItems.select(PasswordItems.title, 
-                        PasswordItems.description, 
-                        PasswordItems.username, 
-                        PasswordItems.password
-                    ).where(PasswordItems.owner == uid).order_by(PasswordItems.created_date).limit(i_number).dicts()
-                items = []
-                for row in query:
-                    items.append(row)
-
-                result = {"number": len(items), "items": items}
-                resp.status = falcon.HTTP_200
-                resp.media = result
-                return
-            else:
-                resp.status = falcon.HTTP_403
-                resp.media = {"code": 12, "message": "Invalid parameter(s)"}
-                return
-        except Exception as e:
-            resp.status = falcon.HTTP_500
-            resp.media = {"code": 1, "message": f"Unknow error : {e}"}
+        if q1 is None:
+            resp.media = {"title": "BAD_REQUEST", "description": "failed to get password items"}
             return
-"""
+        elif len(q1) < 1:
+            resp.media = {"title": "BAD_REQUEST", "description": "empty password items"}
+            return
+        
+        print(q1)
+
+        results: list = []
+        for row in q1:
+            row_dict: dict = {}
+            for i, col in enumerate(columns):
+                row_dict[col.name] = row[i]
+        results.append(row_dict)
+
+        resp.status = falcon.HTTP_OK
+        resp.media  = {"title": "OK", "description": "password list getted successful", "content": results}
