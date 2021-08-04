a401b847ebf8138e69ded5ef38c81316516476f4
Estéban Ristich <esteban.ristich@protonmail.com>
2021-08-04 06:21:57 +0200

    Update TEXT columns for database tables

--- database/sql/tables.sql
+++ database/sql/tables.sql
@@ -8,19 +8,19 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS tester_keys (
     id              VARCHAR(20) PRIMARY KEY NOT NULL UNIQUE,
-    type            TEXT NOT NULL,
+    type            VARCHAR(6) NOT NULL,
     expiration_on   TIMESTAMPTZ DEFAULT NULL,
     created_on      TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 
 CREATE TABLE IF NOT EXISTS accounts (
-    id                  TEXT PRIMARY KEY NOT NULL UNIQUE,
+    id                  VARCHAR(24) PRIMARY KEY NOT NULL UNIQUE,
     f_tester_key        VARCHAR(20) NOT NULL UNIQUE,
-    firstname           TEXT NOT NULL,
-    lastname            TEXT NOT NULL,
-    email               TEXT NOT NULL UNIQUE,
-    password            TEXT NOT NULL,
+    firstname           VARCHAR(20) NOT NULL,
+    lastname            VARCHAR(20) NOT NULL,
+    email               VARCHAR(150) NOT NULL UNIQUE,
+    password            VARCHAR(256) NOT NULL,
     public_key          BYTEA NOT NULL,
     private_key         BYTEA NOT NULL,
     roles               TEXT NOT NULL,
@@ -44,14 +44,14 @@ CREATE TABLE IF NOT EXISTS accounts (
 
 CREATE TABLE IF NOT EXISTS passwords (
     id          UUID PRIMARY KEY NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
-    f_owner     TEXT NOT NULL,
-    type        TEXT NOT NULL DEFAULT 'basic',
-    name        TEXT NOT NULL,
-    description TEXT DEFAULT NULL,
-    login       TEXT DEFAULT NULL,
+    f_owner     VARCHAR(24) NOT NULL,
+    type        VARCHAR(10) NOT NULL DEFAULT 'basic',
+    name        VARCHAR(24) NOT NULL,
+    description VARCHAR(255) DEFAULT NULL,
+    login       VARCHAR(128) DEFAULT NULL,
     password_1  TEXT NOT NULL,
     password_2  TEXT DEFAULT NULL,
-    url         TEXT DEFAULT NULL,
+    url         VARCHAR(255) DEFAULT NULL,
 --    data        JSON NOT NULL, -- '{"name": "N26", "description": "Bank account", "login": "random01", "password": ["root"], "url": "https://app.n26.com/login"}'
     updated_on  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_on  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -63,9 +63,10 @@ CREATE TABLE IF NOT EXISTS passwords (
 
 CREATE TABLE IF NOT EXISTS tags (
     id          UUID PRIMARY KEY NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
-    f_owner     TEXT NOT NULL,
-    name        TEXT NOT NULL,
-    color       TEXT DEFAULT NULL,
+    f_owner     VARCHAR(24) NOT NULL,
+    name        VARCHAR(20) NOT NULL,
+    color       VARCHAR(8) DEFAULT NULL,
+    icon        VARCHAR(40) DEFAULT NULL
     -- See to add icon in relational table maybe
     CONSTRAINT fk_account
         FOREIGN KEY(f_owner)
