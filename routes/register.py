fb542b7b4e7a8910d8d6ad87e019497aef6c50d8
PowerSaucisse <esteban.ristich@protonmail.com>
2021-08-30 00:50:29 +0200

    Update registration to send activation email

--- routes/register.py
+++ routes/register.py
@@ -1,5 +1,5 @@
 import falcon, datetime
-from utils.security.auth import UserPasswordHasher, gen_account_keypair
+from utils.security.auth import EmailAuthToken, UserPasswordHasher, gen_account_keypair
 from utils.config import AppState
 from utils.base import api_validate_form, api_message
 
@@ -7,6 +7,7 @@ class Register(object):
 
     def __init__(self):
         self.password_hasher    = UserPasswordHasher()
+        self._email_token_controller = EmailAuthToken()
         self.post_form          = {
             "$schema": AppState.Tools.JSONSCHEMA_VERSION,
             "type": "object",
@@ -22,9 +23,6 @@ class Register(object):
         }
 
 
-    def on_get(self, req, resp):
-        pass
-
     def on_post(self, req, resp):
         resp.status         = falcon.HTTP_400
 
@@ -32,52 +30,57 @@ class Register(object):
             q1 = None
             q2 = None
             with AppState.Database.CONN.cursor() as cur:
-                cur.execute("SELECT * FROM tester_keys WHERE id = %s AND type = %s AND expiration_on > CURRENT_TIMESTAMP LIMIT 1", (req.media["key"], AppState.TAG))
+                cur.execute("SELECT id FROM tester_keys WHERE id = %s AND type = %s AND expiration_on > CURRENT_TIMESTAMP AND f_owner IS NULL LIMIT 1", (req.media["key"], AppState.TAG))
                 q1 = cur.fetchone()
 
             if q1 is None:
-                api_message('d', "Invalid key")
+                api_message('d', "key invalid, expired or already activated")
                 resp.status = falcon.HTTP_400
-                resp.media = {'title': 'BAD_REQUEST', 'description': 'key not found'}
+                resp.media = {'title': 'BAD_REQUEST', 'description': 'key invalid, expired or already activated'}
                 return
 
             key_id: int = q1[0]
             print(f'key id : {key_id}')
 
             with AppState.Database.CONN.cursor() as cur:
-                cur.execute("SELECT * FROM accounts WHERE id = %s OR email = %s", (req.media['username'], req.media["email"]))
+                cur.execute("SELECT id, email, roles FROM accounts WHERE id = %s OR email = %s", (req.media['username'], req.media["email"]))
                 q2 = cur.fetchone()
 
             if q2 is None:
-                keypair = gen_account_keypair()
-                datenow = datetime.datetime.utcnow()
-
+                # keypair = gen_account_keypair()
+                # datenow = datetime.datetime.utcnow()
                 with AppState.Database.CONN.cursor() as cur:
                     try:
                         cur.execute(
-                            "INSERT INTO accounts (id, firstname, lastname, email, password, public_key, private_key, roles, f_tester_key) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                            "INSERT INTO accounts (id, firstname, lastname, email, password) VALUES (%s, %s, %s, %s, %s)",
                             (
                                 req.media['username'],
                                 req.media['firstname'],
                                 req.media['lastname'],
                                 req.media['email'],
-                                self.password_hasher.hash_password(req.media['password']),
-                                keypair[0],
-                                keypair[1],
-                                "standard",
-                                key_id
+                                self.password_hasher.hash_password(req.media['password'])
                             )
                         )
+                        cur.execute("UPDATE tester_keys SET f_owner = %s WHERE id = %s", (req.media["username"], key_id))
                         AppState.Database.CONN.commit()
                     except Exception as e:
                         AppState.Database.CONN.rollback()
                         api_message("e", f'Failed transaction : {e}')
                         raise falcon.HTTPBadRequest()
-            
+
+                # gen activation token and send it
+                activation_token: str = self._email_token_controller.create(
+                    duration=86400,
+                    uid=req.media["username"],
+                    tester_key=req.media["key"]
+                )
+                activation_link = f'https://quarkey.com/active/account?token={activation_token}'
+                AppState.Email.CONN.send(to=req.media["email"], subject="Quarkey Account Activation", contents=f'Hi {req.media["firstname"]} !\n\nHere is your activation key for your Quarkey account :\n{activation_link}')
         
+                # response success
                 api_message('d', "successfull register (user=\"{0}\", uid={1})".format(req.media['firstname'] + ' ' + req.media['lastname'], req.media['username']))
                 resp.status = falcon.HTTP_201
-                resp.media = {'title': 'CREATED', 'description': 'accounts created successfull'}
+                resp.media = {'title': 'CREATED', 'description': 'accounts created successfull', "content": {"activation_link": f'http://quarkey.herokuapp.com/api/activate/account?token={activation_token}'}}
                 return
             else:
                 resp.status = falcon.HTTP_400
