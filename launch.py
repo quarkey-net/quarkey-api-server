d64e054ad166d4050d0759b5f4d2fef822c5dd3b
PowerSaucisse <esteban.ristich@protonmail.com>
2021-08-30 03:43:31 +0200

    Link enpoints and add init for email controller

--- launch.py
+++ launch.py
@@ -12,12 +12,14 @@ from routes.process_link_tag import ProcessLinkTag
 from routes.process_tester_key import ProcessTesterKey
 from routes.password_item import PasswordItem
 from routes.tool import ProcessGenData
+from routes.process_account_activation import ProcessAccountActivation
+from routes.process_info import ProcessApiInfos
 
 # from routes.verify_account import VerifyAccount
 from routes.middleware import AcceptJSONMiddleware, CORSMiddleware, DebugMiddleware
 
 from utils.config import AppState
-from utils.base import get_rsa_keypair
+from utils.base import get_rsa_keypair, smtp_connect
 
 parser = argparse.ArgumentParser(prog="launch", usage='%(prog)s [options] path', description="configure api launching")
 parser.add_argument('--pub', metavar="pub", type=str, help="Rsa public key file for account token genaration. The program try to get keypair from database if not specified")
@@ -50,11 +52,20 @@ if AppState.TAG in ['dev', 'test']:
 middlewares.append(AcceptJSONMiddleware())
 # middlewares.append(DatabaseConnectMiddleware()) 
 
+# init smtp quarkey email connection
+if not smtp_connect():
+    exit(1)
+
 
 if AppState.AccountToken.TYPE == 'RS256':
-    keypair: list = get_rsa_keypair(token_type="account_authentication")
-    AppState.AccountToken.PUBLIC = keypair[0]
-    AppState.AccountToken.PRIVATE = keypair[1]
+    account_keypair: list = get_rsa_keypair(token_type="account_controller")
+    AppState.AccountToken.PUBLIC_KEY = account_keypair[0]
+    AppState.AccountToken.PRIVATE_KEY = account_keypair[1]
+    
+    email_keypair: list = get_rsa_keypair(token_type="email_controller")
+    AppState.Email.PUBLIC_KEY = email_keypair[0]
+    AppState.Email.PRIVATE_KEY = email_keypair[1]
+
 
 api = application = falcon.App(middleware=middlewares)
 
@@ -66,13 +77,14 @@ pass_itm = PasswordItem()
 
 api.add_route("/api/auth/register", reg)
 api.add_route("/api/auth/login", log)
+api.add_route("/api/activate/account", ProcessAccountActivation())
 api.add_route("/api/account/item/password", pass_itm)
 api.add_route("/api/account/tester/key", ProcessTesterKey())
 api.add_route("/api/account/item/tag", ProcessTag())
 api.add_route("/api/account/item/password/link/to/tag", ProcessLinkTag())
-# tools (only moderator && admin)
+# private (only moderator && admin) and debug endpoint
 api.add_route("/api/account/tool/gen/data", ProcessGenData())
-
+api.add_route("/api/infos", ProcessApiInfos())
 
 if platform.system() == "Linux":
     import bjoern
